08098475e051cc4a8e603178ed1fd67f672ce276
unknown <yoneeki1177@gmail.com>
2023-08-23 11:58:09 +0900

    java

--- src/Main/swingHighNLowMain.java
+++ src/Main/swingHighNLowMain.java
@@ -3,6 +3,6 @@ package Main;
 public class swingHighNLowMain {
 
 	public static void main(String[] args) {
-		swingHighNLow03 hnl = new swingHighNLow03();	
+		swingHighNLow04 hnl = new swingHighNLow04();	
 	}
 }
